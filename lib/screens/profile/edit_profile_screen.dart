04bec69e3ea5ea5924c46c7e6548bee4b678a9b0
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-09-04 21:44:13 +0300

    first project 3

--- lib/screens/profile/edit_profile_screen.dart
+++ lib/screens/profile/edit_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class EditProfileScreen extends StatefulWidget {
   const EditProfileScreen({super.key});
 
@@ -9,6 +10,325 @@ class EditProfileScreen extends StatefulWidget {
 class _EditProfileScreenState extends State<EditProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Edit Profile",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          margin: EdgeInsets.only(left: 8.0, right: 8.0, top: 12),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 90,
+                height: 90,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: CircleAvatar(
+                        child: Image.asset("assets/image 28.png"),
+                        radius: 40,
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: InkWell(
+                          child: Icon(
+                        Icons.camera_alt,
+                        color: Colors.grey,
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Name",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  hintText: " Nourhan Abu Ghaly",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 16,
+                  ),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  contentPadding: EdgeInsets.all(8.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Email Address",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.email_rounded,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  hintText: " Nourhan@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 16,
+                  ),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  contentPadding: EdgeInsets.all(8.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "User name",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.person_2_rounded,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  hintText: " example@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 16,
+                  ),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  contentPadding: EdgeInsets.all(8.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Password",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock_outline_rounded,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  hintText: " example@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 16,
+                  ),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  contentPadding: EdgeInsets.all(8.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Phone",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.account_balance_rounded,
+                                color: Colors.grey,
+                                size: 30,
+                              ),
+                              hintText: "+972 ",
+                              hintStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                              fillColor: Colors.grey[100],
+                              filled: true,
+                              contentPadding: EdgeInsets.all(8.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.black),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              hintText: "563 778965",
+                              hintStyle: TextStyle(
+                                color: Colors.grey[300],
+                                fontSize: 16,
+                              ),
+                              fillColor: Colors.grey[100],
+                              filled: true,
+                              contentPadding: EdgeInsets.all(8.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Birth of Date",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black),
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.calendar_month,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  hintText: " 4/5/2001",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                    fontSize: 16,
+                  ),
+                  fillColor: Colors.grey[100],
+                  filled: true,
+                  contentPadding: EdgeInsets.all(8.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, bottom: 10),
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Upfate",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
